dbf15a4fd2193dab4a551748922f6f027c4fb6b9
Vinicius Duarte <viniciusrduarte8@gmail.com>
2024-09-20 15:43:13 -0300

    AppDashboard

---
+++ AjaxJquery/app_dashboard/script.js
@@ -0,0 +1,34 @@
+$(document).ready(() => {
+	$('#documentacao').on('click', () => {
+        //$('#pagina').load('documentacao.html')
+        $.get('documentacao.html', data => {
+            $('#pagina').html(data)
+        })
+    })
+
+    $('#suporte').on('click', () => {
+        //$('#pagina').load('suporte.html')
+        $.get('suporte.html', data => {
+            $('#pagina').html(data)
+        })
+    })
+
+
+    //ajax
+    $('#competencia').on('change', e => {
+        
+        let competencia = $(e.target).val()
+        
+        $.ajax({
+            type: 'GET',
+            url: 'app.php',
+            data: `competencia=${competencia}`,
+            dataType: 'json',
+            success: dados => {
+                $('#numeroVendas').html(dados.numeroVendas)
+                $('#totalVendas').html(dados.totalVendas)
+            },
+            error: erro => {console.log(erro)}
+        })
+    })
+})
